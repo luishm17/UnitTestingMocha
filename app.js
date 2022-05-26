7176bd81f2b2bb653d0a742af243a0ee5f23ed0f
luismhm17 <luis.hunken17@gmail.com>
2022-05-25 21:34:55 -0500

    feat: Implementacion de contenedor, se agregan mas funcionalidades, y se coloca Readme referente al contenedor y su forma de uso disponible

--- app.js
+++ app.js
@@ -9,14 +9,67 @@ const port = 8080;
 app.get('/functions/even/:number', (req, res) => {
 
   console.log('Looking for ' + req.params.number);
-  //console.log(postalCodesArray);
+
+  let evaluatedNumber = parseInt(req.params.number);
 
   const functions = new functionClass();
-  let resultFunction = functions.isEven();
+  let resultFunction = functions.isEven(evaluatedNumber);
 
-  let postalCodeObject = { result: resultFunction};
+  let response = { result: resultFunction};
 
-  res.send(postalCodeObject);
+  res.send(response);
+});
+
+app.get('/functions/string/:paramString', (req, res) => {
+
+  let evaluatedString = req.params.paramString;
+
+  console.log('Looking for ' + evaluatedString);
+
+  const functions = new functionClass();
+  let resultFunction = functions.getStringLength(evaluatedString);
+
+  let response = { result: resultFunction};
+
+  res.send(response);
+});
+
+app.get('/functions/number-month/:number', (req, res) => {
+
+  let evaluatedNumber = parseInt(req.params.number);
+
+  console.log('Looking for ' + evaluatedNumber);
+
+  const functions = new functionClass();
+  let resultFunction = functions.numberMonth(evaluatedNumber);
+
+  let response = { result: resultFunction};
+
+  res.send(response);
+});
+
+app.get('/functions/get-hour/', (req, res) => {
+
+  const functions = new functionClass();
+  let resultFunction = functions.getHour();
+
+  let response = { result: resultFunction};
+
+  res.send(response);
+});
+
+app.get('/functions/upper-case/:paramString', (req, res) => {
+
+  let evaluatedString = req.params.paramString;
+
+  console.log('Looking for ' + evaluatedString);
+
+  const functions = new functionClass();
+  let resultFunction = functions.upperCase(evaluatedString);
+
+  let response = { result: resultFunction};
+
+  res.send(response);
 });
 
 app.listen(port, hostname, () => {
